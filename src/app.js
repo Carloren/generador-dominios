32f71f6442dc17e38abd05580bcb41114024c8c0
Carlos Lorenzo <carloren.1996@gmail.com>
2025-05-02 10:11:56 +0000

    agrego la complejidad opcional de las extensiones

--- src/app.js
+++ src/app.js
@@ -1,27 +1,34 @@
 import "bootstrap";
 import "./style.css";
-                                                                  // --> Comentar todos los import para probar directamente en la consola y que no de error de extensión de archivo
+// --> Comentar todos los import para probar directamente en la consola y que no de error de extensión de archivo
 
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-let pronouns = ['the', 'our', 'your', 'a', 'his', 'her', 'its'];
-let adjs = ['great', 'big', 'small', 'enormous', 'titanic'];
-let nouns = ['jogger', 'racoon', 'dog', 'monkey', 'cat', 'horse'];
+let pronouns = ['the', 'our', 'your'];
+let adjs = ['great', 'big', 'small', 'titanic'];
+let nouns = ['jokes', 'racom', 'dog', 'monkey', 'cat', 'horses'];
+let exts = ['.com', '.es', '.net', '.org']
 
 function domainGen() {
 
   let domains = '';
 
-  for (let pronoun of pronouns){
-    for (let adj of adjs){
-      for (let noun of nouns){
-        domains += 'www.' + pronoun + adj + noun + ".com\n"
+  for (let pronoun of pronouns) {
+    for (let adj of adjs) {
+      for (let noun of nouns) {
+        for (let ext of exts) {
+          if (noun.endsWith(ext.substring(1))) {
+            domains += 'www.' + pronoun + adj + noun.substring(0, (noun.length - ext.length + 1 )) + ext + '\n' // --> aquí integramos la extensión con el nombre si este termina igual que la extensión
+          } else {
+            domains += 'www.' + pronoun + adj + noun + ext + '\n'
+          }
+        }
       }
     }
   }
 
-  console.log (domains)
+  console.log(domains)
 
   document.getElementById("domain").innerText = domains;          // --> Comentar esta línea para probar directamente en la consola y que no de error de extensión de archivo
 

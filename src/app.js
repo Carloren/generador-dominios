1823daec46b254baa2eb42a5348e7be7ecd38fdb
Carlos Lorenzo <carloren.1996@gmail.com>
2025-05-28 09:43:31 +0000

    mejora con bootstrap y template literal

--- src/app.js
+++ src/app.js
@@ -1,6 +1,5 @@
 import "bootstrap";
 import "./style.css";
-                                                                  // --> Comentar todos los import para probar directamente en la consola y que no de error de extensión de archivo
 
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
@@ -18,10 +17,12 @@ function domainGen() {
     for (let adj of adjs) {
       for (let noun of nouns) {
         for (let ext of exts) {
-          if (noun.endsWith(ext.substring(1))) {                  // --> aquí integramos la extensión con el nombre si este termina igual que la extensión
-            domains += 'www.' + pronoun + adj + noun.substring(0, (noun.length - ext.length + 1 )) + ext + '\n' 
+          if (noun.endsWith(ext.substring(1))) {
+            domains += `www.${pronoun}${adj}${noun.substring(0, (noun.length - ext.length + 1))}${ext}
+            `
           } else {
-            domains += 'www.' + pronoun + adj + noun + ext + '\n'
+            domains += `www.${pronoun}${adj}${noun}${ext}
+            `
           }
         }
       }
@@ -30,9 +31,7 @@ function domainGen() {
 
   console.log(domains)
 
-  document.getElementById("domain").innerText = domains;          // --> Comentar esta línea para probar directamente en la consola y que no de error de extensión de archivo
+  document.getElementById("domain").innerText = domains;
 
-  console.log("Hello Rigo from the console!");
 };
-document.getElementById("button").onclick = domainGen;            // --> Comentar esta línea para probar directamente en la consola y que no de error de extensión de archivo
-// domainGen()                                                    // --> Descomentar esta funcion para probar directamente en la consola y que no de error de extensión de archivo
+document.getElementById("button").onclick = domainGen;
